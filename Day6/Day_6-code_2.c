bd0c253fa6c04f4734a240092e53fdc9fd953111
Kumar Krishn <kr.krishn44@gmail.com>
2026-08-15 23:26:28 +0530

    Add files via upload

---
+++ Day6/Day_6-code_2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+//Q12: Write a program to input an integer and check whether it is positive, negative or zero using nested if–else.
+
+int main(){
+    int integer;
+    printf("Enter an integer:\n");
+    scanf("%d" , &integer);
+    if (integer >= 0){
+        if (integer > 0){
+            printf("The given integer %d is a positive integer.\n" , integer);
+        }
+        else{
+            printf("The given integer is 0.");
+        }
+    }
+    else{
+        printf("The given integer %d is a negative integer.\n" , integer);
+    }
+    return 0;
+
+
+}
