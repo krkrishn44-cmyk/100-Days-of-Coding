898fcc5b544489d9abe875cc49b73fbc4cac3ab0
Kumar Krishn <kr.krishn44@gmail.com>
2026-08-25 18:13:31 +0530

    Add files via upload

---
+++ Day14/Day14_2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int i;
+    int product = 1;
+    
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("The even numbers from 1 to %d are:\n",n);
+
+    for (i = 2; i <= n; i += 2) 
+    {
+        printf("%d\n", i);
+         product *= i;
+    }
+
+    printf("Product of even numbers from 1 to %d = %d\n", n, product);
+
+    return 0;
+}
