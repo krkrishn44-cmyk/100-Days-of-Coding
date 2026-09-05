9bbc7131d6b3590cafe682733569af001a1c0cbd
Kumar Krishn <kr.krishn44@gmail.com>
2026-09-05 23:53:17 +0530

    Add files via upload

---
+++ Day24/Day24_1.c
@@ -0,0 +1,24 @@
+/*Q47: Write a program to print the following pattern:
+*
+**
+***
+****
+*****
+*/
+
+#include<stdio.h>
+
+int main(){
+    int a;
+    int b;
+
+    for(a=1;a<=5;a++)
+    {
+        for(b=1;b<=a;b++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ Day24/Day24_2.c
@@ -0,0 +1,24 @@
+/*Q48: Write a program to print the following pattern:
+1
+12
+123
+1234
+12345
+*/
+
+#include<stdio.h>
+
+int main(){
+    int a;
+    int b;
+    
+    for(a=1;a<=5;a++)
+    {
+        for(b=1;b<=a;b++)
+        {
+            printf("%d",b);
+        }
+        printf("\n");
+    }
+    return 0;
+}
