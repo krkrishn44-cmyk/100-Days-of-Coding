e6da76f061ea0048ecfbb91c14c76f1f36406dd0
Kumar Krishn <kr.krishn44@gmail.com>
2026-09-21 19:13:05 +0530

    Add files via upload

---
+++ Day43/Day43_1.c
@@ -0,0 +1,27 @@
+//Q85: Reverse a string.
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int length = 0;
+    int i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[length] != '\0' && str[length] != '\n') 
+    {
+        length++;
+    }
+    printf("Reversed string: ");
+
+    for (i = length - 1; i >= 0; i--) 
+    {
+        printf("%c", str[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
---
+++ Day43/Day43_2.c
@@ -0,0 +1,33 @@
+//Q86: Check if a string is a palindrome.
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int length = 0;
+    int i;
+    int palindrome = 1;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    while (str[length] != '\0') 
+    {
+        length++;
+    }
+    for (i = 0; i < length / 2; i++) 
+    {
+        if (str[i] != str[length - 1 - i])
+        {
+            palindrome = 0;
+            break;
+        }
+    }
+
+    if (palindrome == 1)
+        printf("The string is a palindrome.\n");
+    else
+        printf("The string is not a palindrome.\n");
+
+    return 0;
+}
